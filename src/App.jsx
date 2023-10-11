935bf12962691f1eed85076ce6f9ef45762e505e
Ben Gillott <benjaminrgillott@gmail.com>
2023-10-11 15:13:29 -0700

    Working Color Shift Material GLSL Shader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls, Plane } from "@react-three/drei";
-// import { shaderMaterial } from "@react-three/drei";
+import { Box, OrbitControls, Plane, shaderMaterial } from "@react-three/drei";
 // import glsl from "babel-plugin-glsl/macro";
+import { extend } from "@react-three/fiber";
+import * as THREE from "three";
+
+const ColorShiftMaterial = shaderMaterial(
+  { time: 0, color: new THREE.Color(0.2, 0.0, 0.1) },
+  // vertex shader
+  /*glsl*/ `
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  // fragment shader
+  /*glsl*/ `
+    uniform float time;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+    }
+  `
+);
+extend({ ColorShiftMaterial });
+
+const TestMaterial = () => {
+  const matRef = useRef();
+  useFrame((state, delta) => {
+    matRef.current.time += 0.03;
+  });
+
+  return <colorShiftMaterial ref={matRef} color='hotpink' time={0} />;
+};
 
 const BoxScene = () => {
   const boxRef = useRef();
   useFrame((state, delta) => {
     boxRef.current.rotation.y += 0.001;
+    boxRef.current.time += 0.001;
   });
 
   return (
     <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-      <meshStandardMaterial color={"orange"} />
+      {/* <meshStandardMaterial color={"orange"} /> */}
+      <TestMaterial />
     </Box>
   );
 };
@@ -32,8 +66,8 @@ const App = () => {
     <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
       <OrbitControls />
       <pointLight position={[10, 10, 10]} />
-      <PlaneScene />
-      {/* <BoxScene /> */}
+      {/* <PlaneScene /> */}
+      <BoxScene />
     </Canvas>
   );
 };
