495138598f3197c6aa294c96a9ce5e1690af0bbe
Ben Gillott <benjaminrgillott@gmail.com>
2023-10-11 13:43:15 -0700

    Working plane with Drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Box, OrbitControls } from "@react-three/drei";
+import { Box, OrbitControls, Plane } from "@react-three/drei";
+// import { shaderMaterial } from "@react-three/drei";
+// import glsl from "babel-plugin-glsl/macro";
 
-const Scene = () => {
+const BoxScene = () => {
   const boxRef = useRef();
   useFrame((state, delta) => {
     boxRef.current.rotation.y += 0.001;
   });
 
   return (
-    <>
-      <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
-        <meshStandardMaterial color={"orange"} />
-      </Box>
-      <pointLight position={[10, 10, 10]} />
-    </>
+    <Box ref={boxRef} args={[1, 1, 1]} rotation={[0.5, 0, 0]}>
+      <meshStandardMaterial color={"orange"} />
+    </Box>
+  );
+};
+
+const PlaneScene = () => {
+  const planeRef = useRef();
+
+  return (
+    <Plane ref={planeRef} args={[2.5, 2.5]}>
+      <meshStandardMaterial color={"orange"} />
+    </Plane>
   );
 };
 
@@ -22,7 +31,9 @@ const App = () => {
   return (
     <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
       <OrbitControls />
-      <Scene />
+      <pointLight position={[10, 10, 10]} />
+      <PlaneScene />
+      {/* <BoxScene /> */}
     </Canvas>
   );
 };
